30c627d66efd4050b00e6b710f8c4b3e3ac8af33
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-10-17 12:39:18 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118058/latihan32daftarfilm/Film.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118058.latihan32daftarfilm;
+
+/**
+ *
+ * @author user
+ */
+public class Film {
+    String nama;
+    String genre;
+    Double rating;
+    int duration;
+    
+    public void nowPlaying(String genre,String nama , Double rating ,int duration){
+     System.out.println("Hallo everyone ");
+     System.out.println("Judul Film: " +nama );
+     System.out.println("Genre Film : "+genre);
+     System.out.println("Rating Film : "+rating);
+     System.out.println("Duration Film : "+duration+ " Menit");
+     System.out.println("");
+     System.out.println("Developed by satyaning andaru");
+    
+    }
+}
+
+
